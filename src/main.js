06f93f06ce67b7983988d322226b7593c40186b6
huihui <huihui@qq.com>
2018-01-21 23:23:20 +0800

    创建项目

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import mui from 'mui'
+
+import '../static/css/mui.css'
+
+
+Vue.config.productionTip = false
+
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+})
